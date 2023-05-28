028dd285cc51b787520682f501ba09bd09fbc3df
Livcamp <info@livcamp.nl>
2023-05-28 12:39:37 +0200

    title in blog url

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #path('blog/', views.blog_overview, name='overview'),
     #path('blogpost/', views.blogpost_view, name='blogpost'),
     path('camperblog/', BlogView.as_view(), name='camperblog'),
-    path('article/<int:pk>', ArticleView.as_view(), name='blogpost'),
+    #path('article/<int:pk>', ArticleView.as_view(), name='blogpost'),
+    path('<slug:slug>', ArticleView.as_view(), name='blogpost'),
     path('archive/', views.blogarchive_view, name='archive'),
 ]
